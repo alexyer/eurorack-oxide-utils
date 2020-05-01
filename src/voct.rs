1d76d3d7637bcdb15a6fb5d6e2185720fed3881f
Olexander Yermakov <olexander.yermakov@gmail.com>
2020-05-01 22:12:16 +0300

    VOct u16 -> f32

--- src/voct.rs
+++ src/voct.rs
@@ -1,13 +1,15 @@
 //! V/Oct conversion methods.
+#[allow(unused_imports)]
+use micromath::F32Ext;
 
 /// V/Oct type.
-pub struct VOct(pub u16);
+pub struct VOct(pub f32);
 
 const C1_FREQ: f32 = 32.703;
 
 impl VOct {
     /// Convert to Hz.
     pub fn hz(&self) -> f32 {
-            C1_FREQ * 2_u16.pow(self.0 as u32 - 1) as f32
+            C1_FREQ * 2_f32.powf(self.0 - 1.0)
     }
 }
