a7a9181273e2bea7b2d223d92244e52e70199809
Olexander Yermakov <olexander.yermakov@gmail.com>
2020-05-02 14:22:53 +0300

    More utility methods for V/Oct

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ mod tests {
     use super::voct::*;
 
     #[test]
-    fn test_hz() {
+    fn test_voct_hz() {
         let cases = [
             (1.0, 32.703),
             (2.0, 65.406),
@@ -24,7 +24,79 @@ mod tests {
             let voct = VOct(*v);
             assert_eq!(voct.hz(), *freq);
         }
+    }
 
+    #[test]
+    fn test_mvoct_hz() {
+        let cases = [
+            (1000.0, 32.703),
+            (2000.0, 65.406),
+            (3000.0, 130.812),
+            (4000.0, 261.624),
+            (5000.0, 523.248),
+        ];
 
+        for (v, freq) in &cases {
+            let voct = MvOct(*v);
+            assert_eq!(voct.hz(), *freq);
+        }
+    }
+
+    #[test]
+    fn test_voct_ms() {
+        let cases = [(1.0, 30), (2.0, 15), (3.0, 7), (4.0, 3), (5.0, 1)];
+
+        for (v, period) in &cases {
+            let voct = VOct(*v);
+            assert_eq!(voct.ms(), *period);
+        }
+    }
+
+    #[test]
+    fn test_voct_us() {
+        let cases = [
+            (1.0, 30578),
+            (2.0, 15289),
+            (3.0, 7644),
+            (4.0, 3822),
+            (5.0, 1911),
+        ];
+
+        for (v, period) in &cases {
+            let voct = VOct(*v);
+            assert_eq!(voct.us(), *period);
+        }
+    }
+
+    #[test]
+    fn test_mvoct_ms() {
+        let cases = [
+            (1000.0, 30),
+            (2000.0, 15),
+            (3000.0, 7),
+            (4000.0, 3),
+            (5000.0, 1),
+        ];
+
+        for (v, period) in &cases {
+            let voct = MvOct(*v);
+            assert_eq!(voct.ms(), *period);
+        }
+    }
+
+    #[test]
+    fn test_mvoct_us() {
+        let cases = [
+            (1000.0, 30578),
+            (2000.0, 15289),
+            (3000.0, 7644),
+            (4000.0, 3822),
+            (5000.0, 1911),
+        ];
+
+        for (v, freq) in &cases {
+            let voct = MvOct(*v);
+            assert_eq!(voct.us(), *freq);
+        }
     }
 }
--- src/voct.rs
+++ src/voct.rs
@@ -1,15 +1,103 @@
 //! V/Oct conversion methods.
+
+use core::ops;
 #[allow(unused_imports)]
 use micromath::F32Ext;
 
 /// V/Oct type.
+#[derive(Debug)]
 pub struct VOct(pub f32);
+
+/// mV/Oct Type.
+#[derive(Debug)]
+pub struct MvOct(pub f32);
 
 const C1_FREQ: f32 = 32.703;
 
 impl VOct {
     /// Convert to Hz.
     pub fn hz(&self) -> f32 {
-            C1_FREQ * 2_f32.powf(self.0 - 1.0)
+        C1_FREQ * 2_f32.powf(self.0 - 1.0)
+    }
+
+    /// Convert to ms.
+    pub fn ms(&self) -> u32 {
+        (1000.0 / self.hz()) as u32
+    }
+
+    /// Convert to ms.
+    pub fn us(&self) -> u32 {
+        (1000000.0 / self.hz()) as u32
     }
 }
+
+impl MvOct {
+    /// Convert to Hz.
+    pub fn hz(self) -> f32 {
+        VOct::from(self).hz()
+    }
+
+    /// Convert to ms.
+    pub fn ms(self) -> u32 {
+        VOct::from(self).ms()
+    }
+
+    /// Convert to us.
+    pub fn us(self) -> u32 {
+        VOct::from(self).us()
+    }
+}
+
+impl From<MvOct> for VOct {
+    fn from(mv: MvOct) -> Self {
+        Self(mv.0 / 1000.0)
+    }
+}
+
+impl From<VOct> for MvOct {
+    fn from(mv: VOct) -> Self {
+        Self(mv.0 * 1000.0)
+    }
+}
+
+/// Macro to implement arithmetic operations (e.g. multiplication, division)
+/// for wrapper types.
+macro_rules! impl_arithmetic {
+    ($wrapper:ty, $wrapped:ty) => {
+        impl ops::Mul<$wrapped> for $wrapper {
+            type Output = Self;
+            fn mul(self, rhs: $wrapped) -> Self {
+                Self(self.0 * rhs)
+            }
+        }
+
+        impl ops::MulAssign<$wrapped> for $wrapper {
+            fn mul_assign(&mut self, rhs: $wrapped) {
+                self.0 *= rhs;
+            }
+        }
+
+        impl ops::Div<$wrapped> for $wrapper {
+            type Output = Self;
+            fn div(self, rhs: $wrapped) -> Self {
+                Self(self.0 / rhs)
+            }
+        }
+
+        impl ops::Div<$wrapper> for $wrapper {
+            type Output = $wrapped;
+            fn div(self, rhs: $wrapper) -> $wrapped {
+                self.0 / rhs.0
+            }
+        }
+
+        impl ops::DivAssign<$wrapped> for $wrapper {
+            fn div_assign(&mut self, rhs: $wrapped) {
+                self.0 /= rhs;
+            }
+        }
+    };
+}
+
+impl_arithmetic!(VOct, f32);
+impl_arithmetic!(MvOct, f32);
